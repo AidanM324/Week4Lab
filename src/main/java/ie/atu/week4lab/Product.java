c67ac01b915449a7c3195cde28f825fe3452401f
ie.atu <G00423306@atu.ie>
2024-10-09 14:19:37 +0100

    New Project

--- src/main/java/ie/atu/week4lab/Product.java
+++ src/main/java/ie/atu/week4lab/Product.java
@@ -1,6 +1,19 @@
 package ie.atu.week4lab;
 
-import lombok
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+    @NotBlank(message="You must enter a name")
+    private String name;
+    @Min(value=1, message="Must be greater or equal to 1")
+    private int id;
 }
