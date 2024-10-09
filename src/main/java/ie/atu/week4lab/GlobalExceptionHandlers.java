75634b9e4ef65f33850d43938497569a127c1f56
ie.atu <G00423306@atu.ie>
2024-10-09 15:05:22 +0100

    Changes to Exception handling + created person class

--- src/main/java/ie/atu/week4lab/GlobalExceptionHandlers.java
+++ src/main/java/ie/atu/week4lab/GlobalExceptionHandlers.java
@@ -1,15 +1,26 @@
 package ie.atu.week4lab;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandlers {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public String showErrors()
+    public ResponseEntity<Map<String, String>> showErrors(MethodArgumentNotValidException ex)
     {
-        return "errors";
+        Map<String, String> errorList = new HashMap<>();
+        for(FieldError error : ex.getFieldErrors()) {
+            String err_name = error.getField();
+            String err_message = error.getDefaultMessage();
+            errorList.put(err_name, err_message);
+        }
+        return ResponseEntity.status(400).body(errorList);
     }
 }
--- src/main/java/ie/atu/week4lab/Product.java
+++ src/main/java/ie/atu/week4lab/Product.java
@@ -1,6 +1,7 @@
 package ie.atu.week4lab;
 
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -14,6 +15,16 @@ import lombok.NoArgsConstructor;
 public class Product {
     @NotBlank(message="You must enter a name")
     private String name;
+    @NotBlank(message="You must enter a title")
+    private String title;
     @Min(value=1, message="Must be greater or equal to 1")
     private int id;
+    @Min(value=1, message="Must be greater or equal to 1")
+    private int age;
+    @Email(message="You must enter a valid E-mail")
+    private String email;
+    @NotBlank(message="You must enter your position")
+    private String position;
+    @NotBlank(message="You must enter your department")
+    private String department;
 }
